b010d95d0e5dd9e5a3f0d6356878d0c00f9df408
DGLOliveira <dgloliveira90@gmail.com>
2025-10-23 22:10:03 +0000

    Created new Model_List file to track multiple models; Created urlControler.get function to get url parameters on first load; Initiated setInitModel function to validade and update values from urlControler.get

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React, { useState, useEffect, lazy, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar.tsx";
 import Menu from "./components/Menu.tsx";
 import Scenario from "./components/Scenario.js";
-import urlControler from "./controllers/url.js";
+import urlControler from "./controllers/url.ts";
+import ModelList from "./data/Model_List.json"
 import "./styles.css";
-import { time } from "console";
 
 export default function App() {
 
-  const [menu, setMenu]: [string, React.Dispatch<React.SetStateAction<string>>]
+  const DEFAULT_MODEL = ModelList[0]
+
+  const [menu, setMenu]: [string,
+    React.Dispatch<React.SetStateAction<string>>]
     = useState("none")
 
   const [model, setModel]: [{ [key: string]: string },
     React.Dispatch<React.SetStateAction<{ [key: string]: string }>>]
-    = useState({
-      "name": "Canvas",
-      "category": "Sneaker",
-      "dataFile": "Canvas_Sneaker.json"
-    })
+    = useState({})
 
   const [colors, setColors]: [{ [key: string]: string },
     React.Dispatch<React.SetStateAction<{ [key: string]: string }>>]
@@ -27,33 +26,74 @@ export default function App() {
     React.Dispatch<React.SetStateAction<{ [key: string]: boolean }>>]
     = useState({})
 
-  const importData = async () => {
-    const response = await import(`./data/${model.dataFile}`).catch(console.error)
+  //Import model data
+  const importData = async (initModel: { [key: string]: string }) => {
+    const response = await import(`./data/${initModel.dataFile}`).catch(console.error)
     let newColors: { [key: string]: string } = {}
-    Object.keys(response.modifiers.materials).map((key) => {
-      newColors[key] = response.modifiers.materials[key].color
-    })
     let newExtras: { [key: string]: boolean } = {}
-    Object.keys(response.modifiers.meshes).map((key) => {
-      newExtras[key] = response.modifiers.meshes[key].visible
-    })
-    setColors(newColors)
-    setExtras(newExtras)
-
+    if (response) {
+      Object.keys(response.modifiers.materials).map((key) => {
+        newColors[key] = response.modifiers.materials[key].color
+      })
+      Object.keys(response.modifiers.meshes).map((key) => {
+        newExtras[key] = response.modifiers.meshes[key].visible
+      })
+    }
+    return { colors: newColors, extras: newExtras }
   }
 
-  useEffect(() => {
-    importData()
-  }, [model])
+  const setInitModel = async () => {
+    let params = urlControler.prototype.get()
+    let initModel = {}
+    //Check if URL has Category and Model values and they exist in ModelList
+    if (params.Category && params.Model) {
+      let found = false
+      ModelList.map((item) => {
+        if (item.Category === params.Category && item.Name === params.Model) {
+          setModel(item)
+          initModel = item
+          found = true
+        }
+      })
+      //If URL values don't exist in ModelList, use default
+      if (!found) {
+        initModel = DEFAULT_MODEL
+      }
+    }
+    //If URL values don't exist, use default
+    else {
+      initModel = DEFAULT_MODEL
+    }
+    let defaultValues = await importData(initModel)
+    /*
+      Create code for validating and assigning url param values
+    */
+    setModel(initModel)
+    setColors(defaultValues.colors)
+    setExtras(defaultValues.extras)
+  }
 
+  //Runs on page load
   useEffect(() => {
-    //changes URL to new values, pushes to history and prevents flooding history with rapidly changing params
-    const timeout = setTimeout(() => {
-      if (model && colors && extras)
-        urlControler.prototype.update(model, colors, extras)
-    }, 500)
-    if(timeout){
-      return () => clearTimeout(timeout)
+    setInitModel()
+  }, [])
+
+  //Runs on model change, only one model currently available so no current need for this
+  /*useEffect(() => {
+    importData(model)
+  }, [model])*/
+
+  // Updates URL to new values, pushes to history
+  // Note: Timeout prevents flooding history with rapidly changing params inputs
+  useEffect(() => {
+    if (model.Category && model.Name) {
+      const timeout = setTimeout(() => {
+        if (model && colors && extras)
+          urlControler.prototype.update(model, colors, extras)
+      }, 500)
+      if (timeout) {
+        return () => clearTimeout(timeout)
+      }
     }
   }, [model, colors, extras])
 
@@ -70,10 +110,10 @@ export default function App() {
         extras={extras}
         setExtras={setExtras}
       />
-      <Scenario
+      {model && colors && extras && <Scenario
         colors={colors}
         extras={extras}
-      />
+      />}
     </>
   );
 }
--- src/controllers/url.ts
+++ src/controllers/url.ts
@@ -1,24 +1,34 @@
 export default class urlController {
 
     // Runs on page load
-    init() {
+    get() {
+        let params = new URLSearchParams(window.location.search)
+        let paramObj : {[key: string]: string} = {}
+        for(const [key, value] of params) {
+            paramObj[key] = value
+        }
+        return paramObj
     }
 
     // Updates URL with new values and pushes to history
-    update(model, colors, extras) {
+    update(
+        model : {[key: string]: string},
+        colors : {[key: string]: string}, 
+        extras : {[key: string]: boolean}) 
+        {
         let params = new URLSearchParams(window.location.search)
         //Clear all keys
         for( const [key, _value] of params) {
             params.delete(key)
         }
         //Create new URL
-        params.set('Category', model.category);
-        params.set('Model', model.name);
+        params.set('Category', model.Category);
+        params.set('Name', model.Name);
         Object.keys(colors).map((name) => {
             params.set(name, colors[name])
         })
         Object.keys(extras).map((name) => {
-            params.set(name, extras[name])
+            params.set(name, (extras[name]) ? "true" : "false")
         })
         window.history.pushState({}, '', `?${params.toString()}`)
     }
