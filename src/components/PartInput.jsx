ab31678eb26659f9af6a57e02e098cc79ebfe32d
DGLOliveira <dgloliveira90@gmail.com>
2025-12-13 20:37:58 -0100

    Part input now converts changed colors back to hex, in order to keep share link consistent

--- src/components/PartInput.jsx
+++ src/components/PartInput.jsx
@@ -29,6 +29,10 @@ export default function partInput(props) {
             : null;
     }
 
+    const rgbToHex = (rgb) => {
+        return "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1)
+    };
+
     //Converts RGB to HSL
     const rgbToHsl = (rgb) => {
         let r = rgb[0],
@@ -68,6 +72,48 @@ export default function partInput(props) {
         l = (l * 100).toFixed(0);
         let hsl = [h, s + '%', l + '%'];
         return hsl;
+    }
+
+    const hslToRgb =(hsl) => {
+        let h = hsl[0],
+            s = hsl[1] / 100,
+            l = hsl[2] / 100,
+            c = (1 - Math.abs(2 * l - 1)) * s,
+            x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+            m = l - c / 2,
+            r = 0,
+            g = 0,
+            b = 0;
+        if (0 <= h && h < 60) {
+            r = c;
+            g = x;
+            b = 0;
+        } else if (60 <= h && h < 120) {
+            r = x;
+            g = c;
+            b = 0;
+        } else if (120 <= h && h < 180) {
+            r = 0;
+            g = c;
+            b = x;
+        } else if (180 <= h && h < 240) {
+            r = 0;
+            g = x;
+            b = c;
+        } else if (240 <= h && h < 300) {
+            r = x;
+            g = 0;
+            b = c;
+        } else if (300 <= h && h < 360) {
+            r = c;
+            g = 0;
+            b = x;
+        }
+        r = Math.round((r + m) * 255);
+        g = Math.round((g + m) * 255);
+        b = Math.round((b + m) * 255);
+        let rgb = [r, g, b];
+        return rgb
     }
 
     //Updates hue, saturation and luminance when mouse hovers over a different part of the shoe
@@ -191,7 +237,8 @@ export default function partInput(props) {
 
     //Updates colors object when hue, saturation, or lightness changes
     useEffect(() => {
-        setColors({...colors, [hover]: `hsl(${hue},${saturation}%,${lightness}%)`})
+        //Colors are formatted back to hex string in order to keep url short
+        setColors({...colors, [hover]: rgbToHex(hslToRgb([hue, saturation, lightness]))})
     },[hue, saturation, lightness])
 
     return (
